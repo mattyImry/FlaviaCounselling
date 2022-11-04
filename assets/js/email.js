ffb2d25fa3fc141342bab1d922ebec6cf20300e0
mattyImry <matteo.imeri@gmail.com>
2022-11-04 10:44:03 +0000

    Connected emailJs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,11 @@
 console.log('works');
+
+function sendMail(contactForm) {
+    emailjs.send("on_line_psycology", "contact_fcigolla",{
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "more_info": contactForm.message.value,
+        
+    })
+    .then(alert(`Thank you! Your message was sent`));
+}
