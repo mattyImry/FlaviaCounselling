3c13483b191b1a4cd4f3997fdddb952019103ae6
mattyImry <matteo.imeri@gmail.com>
2022-11-09 15:16:46 +0000

    setting map marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,30 +1,44 @@
 console.log('map works!')
 
-var map;
-var service;
-var infowindow;
-
 function initMap() {
-  var practice_fla = new google.maps.LatLng(51.5254613304083, -0.09635282977019068);
 
-  infowindow = new google.maps.InfoWindow();
+    const practice_fla = new google.maps.LatLng(51.5254613304083, -0.09635282977019068);
 
-  map = new google.maps.Map(
-      document.getElementById('map'), {center: practice_fla, zoom: 15});
+    const map = new google.maps.Map( document.getElementById('map'), {
+        center: practice_fla,
+        zoom: 15});
+        
+    const contentString = `<h4> Dr. Flavia Cigolla  </h4>` ;
+        
+    const marker = google.maps.Marker({
+        position: practice_fla,
+        map,
+        title: "Dr Flavia Cigolla",
+    });
+    
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 500
+    });
 
-  var request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
-  };
+    marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+    })
 
-  var service = new google.maps.places.PlacesService(map);
+  
+//   var service = new google.maps.places.PlacesService(map);
 
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+//   service.findPlaceFromQuery(request, function(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       for (var i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
 }
+
+window.initMap = initMap;
