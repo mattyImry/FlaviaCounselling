102a201aa206cdf18387bec0a886c89f5b47e80d
mattyImry <matteo.imeri@gmail.com>
2022-11-11 12:22:49 +0000

    Style infowindow map

--- assets/js/map.js
+++ assets/js/map.js
@@ -8,7 +8,16 @@ function initMap() {
         center: practice_fla,
         zoom: 15});
         
-    const contentString = `<h4> Dr. Flavia Cigolla  </h4>` ;
+    const contentString = `     <div class="infowindow"><h2 style="text-align: center;"> Dr. Flavia Cigolla </h2> <br>
+                                <h3 style="margin-bottom: 5px;">45 Central Street
+                                London
+                                EC1V 8AB  
+                                </h3>
+                                <h4><a  href="https://www.google.com/maps/dir//45+Central+St,+London+EC1V+8AB/@51.5254993,-0.1314781,13z/           data=!4m8!4m7!1m0!1m5!1m1!1s0x48761b57875c84a9:0x2b96d66223772f80!2m2!1d-0.0964588!2d51.5254511" target="_blank">Direction</a>
+                                </h4>
+                                </div>`
+                                
+                                ;
         
     const marker = new google.maps.Marker({
         position: practice_fla,
@@ -18,15 +27,15 @@ function initMap() {
     
     const infowindow = new google.maps.InfoWindow({
         content: contentString,
-        maxWidth: 500
+        maxWidth: 500,
+        
     });
 
-    marker.addListener("click", () => {
-        infowindow.open({
-          anchor: marker,
-          map,
-        });
-    })
+    // marker.addListener("click", () => {
+    //     window.location.href = this.url;
+    // })
+
+    infowindow.open(map, marker);
 
   
 //   var service = new google.maps.places.PlacesService(map);
