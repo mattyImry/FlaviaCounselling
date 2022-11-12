c9a8bfce20ce1b6512d9d4c71b21cc9845a6e93a
mattyImry <matteo.imeri@gmail.com>
2022-11-12 11:06:16 +0000

    Fix font awsome bug

--- assets/js/map.js
+++ assets/js/map.js
@@ -8,13 +8,15 @@ function initMap() {
         center: practice_fla,
         zoom: 15});
         
-    const contentString = `     <div class="infowindow"><h2 style="text-align: center;"> Dr. Flavia Cigolla </h2> <br>
-                                <h3 style="margin-bottom: 5px;">45 Central Street
-                                London
-                                EC1V 8AB  
-                                </h3>
-                                <h4><a  href="https://www.google.com/maps/dir//45+Central+St,+London+EC1V+8AB/@51.5254993,-0.1314781,13z/           data=!4m8!4m7!1m0!1m5!1m1!1s0x48761b57875c84a9:0x2b96d66223772f80!2m2!1d-0.0964588!2d51.5254511" target="_blank">Direction</a>
-                                </h4>
+    const contentString = `     <div class="infowindow">
+                                    <h2 style="text-align: center;"> Dr. Flavia Cigolla </h2> <br>
+                                    <h3 style="margin-bottom: 5px;">45 Central Street
+                                        London
+                                        EC1V 8AB  
+                                    </h3>
+                                    <h4>
+                                        <a  href="https://www.google.com/maps/dir//45+Central+St,+London+EC1V+8AB/@51.5254993,-0.1314781,13z/           data=!4m8!4m7!1m0!1m5!1m1!1s0x48761b57875c84a9:0x2b96d66223772f80!2m2!1d-0.0964588!2d51.5254511" target="_blank"> <i class="fa-solid fa-location-arrow" style="margin-right: 5px;"></i>Direction</a>
+                                    </h4>
                                 </div>`
                                 
                                 ;
