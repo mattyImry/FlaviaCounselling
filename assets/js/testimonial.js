9bc3c0a74ed0ae83dee43a05bd10b766a34c4cc1
Matteo Imeri <matteo.imeri@gmail.com>
2023-02-24 11:44:33 +0000

    add testimonial

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,20 +1,20 @@
 
-//     pagenum = 1;
-//     function AutoRotate() {
+    pagenum = 1;
+    function AutoRotate() {
       
-//        var allElements = document.getElementsByTagName('label');
-//        for (var i = 0, n = allElements.length; i < n; i++) {
-//            var myfor = allElements[i].getAttribute('for');
-//            if ((myfor !== null) && (myfor == ('slide_2_' + pagenum))) {
-//                allElements[i].click();
-//                break;
-//            }
-//        }
-//        if (pagenum == 4) {
-//            pagenum =+ 1;
-//        } else {
-//            pagenum++;
-//        }
-//     }
-//     setInterval(AutoRotate, 4500);
-// ;
+       var allElements = document.getElementsByTagName('label');
+       for (var i = 0, n = allElements.length; i < n; i++) {
+           var myfor = allElements[i].getAttribute('for');
+           if ((myfor !== null) && (myfor == ('slide_2_' + pagenum))) {
+               allElements[i].click();
+               break;
+           }
+       }
+       if (pagenum == 4) {
+           pagenum =+ 1;
+       } else {
+           pagenum++;
+       }
+    }
+    setInterval(AutoRotate, 4000);
+;
